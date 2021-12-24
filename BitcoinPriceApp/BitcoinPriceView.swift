b664c8a3450612402aa5b9cb60228664cdd770ce
piktamine <49212598+piktamine@users.noreply.github.com>
2021-12-23 19:24:21 -0600

    V0.1

---
+++ BitcoinPriceApp/BitcoinPriceView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  BitcoinPriceApp
+//
+//  Created by Admin on 12/23/21.
+//
+
+import SwiftUI
+
+extension Color{
+    static let bitcoinGreen: Color = Color.green.opacity(0.9)
+}
+
+struct BitcoinPriceView: View {
+    
+    @ObservedObject var viewModel: BitcoinPriceViewModel
+    @State private var selectedCurrency: Currency = .usd
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct PriceDeatilsView: View{
+    let priceDetails = PriceDetails
+    
+    var body: some View {
+        ZStack{
+            Color.bitcoinGreen
+            VStack{
+                Text(priceDetails.currency.icon)
+                    .font(.largeTittle)
+                Text("1 Bitcoin =")
+                    .bold()
+                    .font(.title2)
+                Text("\(priceDetails.rate) \(priceDetails.currency.code)")
+                    .bold()
+                    .font(.largeTitle)
+            }
+        }
+    }
+}
+
