da6c0c8e35af513b816e2f2c2be8e8c1e8f7434f
piktamine <49212598+piktamine@users.noreply.github.com>
2021-12-23 18:46:11 -0600

    Initial Commit

---
+++ BitcoinPriceApp/BitcoinPriceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinPriceAppApp.swift
+//  BitcoinPriceApp
+//
+//  Created by Admin on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinPriceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
