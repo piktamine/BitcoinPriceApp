1e063c6be654140f9da5326e0302b4db985600dd
piktamine <49212598+piktamine@users.noreply.github.com>
2021-12-23 18:54:49 -0600

    V0.01

---
+++ BitcoinPriceApp/PriceDetails.swift
@@ -0,0 +1,43 @@
+//
+//  PriceDetails.swift
+//  BitcoinPriceApp
+//
+//  Created by Admin on 12/23/21.
+//
+
+import Foundation
+
+enum Currency: CaseIterable {
+    case usd //
+    case gbp //libras
+    case eur
+    
+    var icon: String{
+        switch self {
+        case .usd:
+            return "💵"
+        case .gbp:
+            return "💷"
+        case .eur:
+            return "💶"
+        }
+    }
+    
+    var code: String{
+        switch self {
+        case .usd:
+            return "USD"
+        case .gbp:
+            return "GBP"
+        case .eur:
+            return "EUR"
+        }
+    }
+}
+
+struct PriceDetails {
+    let currency: Currency
+    let rate: String
+    
+    //public init(currency: Currency, rate: String = "--")
+}
