5ba227c0a56c5a966084d8d5a160ed3abe178384
piktamine <49212598+piktamine@users.noreply.github.com>
2022-01-13 14:52:37 -0600

    V1

--- BitcoinPriceApp/BitcoinPriceViewModel.swift
+++ BitcoinPriceApp/BitcoinPriceViewModel.swift
@@ -14,7 +14,26 @@ class BitcoinPriceViewModel: ObservableObject {
             PriceDetails(currency: $0)
     }
     
+    private var subscription: AnyCancellable?
+    
     public func onAppear() {
         //Obtener los datos del API
+        subscription = CoindeskAPIService.shared.fetchBitcoinPrice()
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    print(error.localizedDescription)
+                case .finished:
+                    print("success")
+                }
+            }, receiveValue: { [weak self] value in
+                guard let self = self else { return }
+                self.lastUpdated = value.time.updated
+                self.priceDetails = [
+                    PriceDetails(currency: .usd, rate: value.bpi.USD.rate),
+                    PriceDetails(currency: .gbp, rate: value.bpi.GBP.rate),
+                    PriceDetails(currency: .eur, rate: value.bpi.EUR.rate),
+                ]
+            })
     }
 }
